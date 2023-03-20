f0136b1f71a8777c64f8a2aa0626b06b96438e7d
Francisco Perez <Franciscop693@gmail.com>
2023-03-20 14:23:00 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(event = 'roller-skate'){
+    return `This Saturday, I want to ${event}!`
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(value = '*'){
+    return function(parameter = 'special'){
+        return `You are ${value}${parameter}${value}!`
+    }
+}
